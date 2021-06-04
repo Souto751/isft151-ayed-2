362f68c4cb7baf3a3d836b2c334ac961900d6b55
Agustin Souto <agus.souto75@gmail.com>
2021-06-04 12:41:05 -0300

    Ahora en Cloud

---
+++ dos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int message = "Hello World!";
+
+    cout << message << endl;
+
+    cin.get();
+    cin.ignore();
+
+    return 0;
+}
