1efac0d6cbc80cf4d99b2702b21966755ac4966b
Agustin Souto <agus.souto75@gmail.com>
2021-06-08 15:57:28 -0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# isft151-ayed-2
-
-Repo para la materia AyED II 2021
--- docs/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-    return (
-        <div className="header">
-            <h1>AyED II</h1>
-        </div>
-    )
-}
